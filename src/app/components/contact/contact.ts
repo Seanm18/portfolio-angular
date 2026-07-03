a98baf315895180f32f543f54d74c56dbee1cd25
Sean <bresduar@gmail.com>
2026-07-02 22:07:20 -0600

    modificacion backend para recibir correos

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
+  styleUrl: './contact.css'
 })
-export class Contact {}
+export class ContactComponent {
+
+  sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_ojnpyuu',
+      'template_pv4fvbo',
+      e.target as HTMLFormElement,
+      'JA24JiIonSxQxJDCQ'
+    ).then(() => {
+      alert('Mensaje enviado correctamente');
+    }).catch(() => {
+      alert('Error al enviar el mensaje');
+    });
+  }
+}
